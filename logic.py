643b49beaf9e15dc70889c9cee0d37650affb79d
ShayTanoS <maks.shaytan2017@gmail.com>
2023-05-05 17:59:18 +0300

    class Ship

--- logic.py
+++ logic.py
@@ -4,3 +4,29 @@ class Ship:
         self.__tp = tp
         self.__x = x
         self.__y = y
+        self.__is_move = True
+        self.__cells = [1 for _ in range(self.__length)]
+
+    def set_start_coords(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def get_start_coords(self):
+        return (self.__x, self.__y)
+
+    def move(self, x, y):
+        pass
+
+    def is_collide(self):
+        pass
+
+    def __getitem__(self, item):
+        return self.__cells[item]
+
+    def __setitem__(self, key, value):
+        self.__cells[key] = value
+
+
+class GamePole:
+    def __init__(self):
+        self.__ships = []
